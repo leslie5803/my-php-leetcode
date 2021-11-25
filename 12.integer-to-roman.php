61394f675a88dc5820c270b6eb9e5139c5d0ba23
Zeno Li <zubin@xinmem.com>
2021-11-25 17:26:18 +0800

    Integer to Roman修改为循环

--- 12.integer-to-roman.php
+++ 12.integer-to-roman.php
@@ -6,58 +6,56 @@
  */
 
 // @lc code=start
-class Solution {
+class Solution
+{
 
     /**
      * @param Integer $num
      * @return String
      */
-    function intToRoman($num) {
+    public function intToRoman($num)
+    {
         $roman = '';
-        
-        $i = intval($num / 1000);
-        $roman .= str_repeat('M', $i);
-        $num = $num % 1000;
-        $i = intval($num / 100);
-        if($i === 9) {
-            $roman .= 'CM';
-        }else if ($i === 4){
-            $roman .= 'CD';
-        }else {
-            if($i >= 5) {
-                $roman .= 'D' . str_repeat('C', $i % 5);
-            }else {
-                $roman .= str_repeat('C', $i % 5);
+
+        $nine   = ['', 'CM', 'XC', 'IX'];
+        $four   = ['', 'CD', 'XL', 'IV'];
+        $greate = ['', 'D', 'L', 'V'];
+        $less   = ['M', 'C', 'X', 'I'];
+        $steps  = [1000, 100, 10, 1];
+
+        $i = 0;
+        while ($i < 4) {
+            $x = intval($num / $steps[$i]);
+            if ($x < 1) {
+                $i++;
+                continue;
             }
-        }
-        $num = $num % 100;
-        $i = intval($num / 10);
-        if($i === 9) {
-            $roman .= 'XC';
-        }else if($i === 4) {
-            $roman .= 'XL';
-        } else {
-            if($i >= 5) {
-                $roman .= 'L' . str_repeat('X', $i % 5);
-            }else {
-                $roman .= str_repeat('X', $i % 5);
+            $num = $num % $steps[$i];
+
+            $times = $x % 5;
+            if ($times === 4) {
+                if ($x > 5) {
+                    $roman .= $nine[$i];
+                    $i++;
+                    continue;
+                }
+
+                $roman .= $four[$i];
+                $i++;
+                continue;
             }
-        }
-        $i = $num % 10;
-        if($i === 9) {
-            $roman .= 'IX';
-        }else if($i === 4) {
-            $roman .= 'IV';
-        }else {
-            if($i >= 5) {
-                $roman .= 'V' . str_repeat('I', $i % 5);
-            }else {
-                $roman .= str_repeat('I', $i % 5);
+
+            if ($x >= 5) {
+                $roman .= $greate[$i] . str_repeat($less[$i], $times);
+                $i++;
+                continue;
             }
+
+            $roman .= str_repeat($less[$i], $times);
+            $i++;
         }
 
         return $roman;
     }
 }
 // @lc code=end
-
