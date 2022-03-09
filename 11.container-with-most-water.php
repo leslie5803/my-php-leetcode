c07f965bf7ebb945297f83eab93e8a588e73194b
Zeno Li <zubin@xinmem.com>
2022-03-09 10:03:44 +0800

    删掉注释

--- 11.container-with-most-water.php
+++ 11.container-with-most-water.php
@@ -18,11 +18,7 @@ class Solution {
         $area = 0;
         $l = 0;
         $r = $len - 1;
-        // for($i = 0, $len1 = $len - 1; $i < $len1; $i++) {
-        //     for($j = $len; $j > $i; $j--) {
-        //         $area = max(min($height[$i], $height[$j]) * ($j - $i), $area);
-        //     }
-        // }
+
         while($l < $r) {
             $area = max($area, min($height[$l], $height[$r]) * ($r - $l));
             if($height[$l] < $height[$r]) {
