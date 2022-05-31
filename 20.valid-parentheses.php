648e2f61e2a6e8e94ba81548d266872cd0af5fbe
Zeno Li <zubin@xinmem.com>
2022-05-31 09:39:33 +0800

    Valid Parentheses

--- 20.valid-parentheses.php
+++ 20.valid-parentheses.php
@@ -12,52 +12,78 @@ class Solution {
      * @param String $s
      * @return Boolean
      */
-    function isValid($s) {
-        if(strlen($s) % 2) {
-            return false;
-        }
+//     function isValid($s) {
+//         if(strlen($s) % 2) {
+//             return false;
+//         }
+// 
+//         while(true){
+//             $c = $s[0];
+//             $s = substr($s, 1);
+//             $i = $this->match($c, $s);
+//             if($i === false){
+//                 return false;
+//             }
+// 
+//             $s = substr($s, 0, $i) . substr($s, $i + 1);
+//             if(empty($s)){
+//                 break;
+//             }
+//         }
+// 
+//         return true;
+//     }
+// 
+//     function match($c, $s)
+//     {
+//         $map = [
+//             '(' => ')',
+//             '{' => '}',
+//             '[' => ']'
+//         ];
+// 
+//         if($s[0] === $map[$c]) {
+//             return 0;
+//         }
+// 
+//         $offset = 0;
+//         for($i = 0, $len = strlen($s); $i < $len; $i++) {
+//             if(in_array($s[$i], ['(', '{', '['], true)) {
+//                 $offset += 2;
+//             }
+// 
+//             if($s[$i] === $map[$c] && $offset === $i) {
+//                 return $offset;
+//             }
+//         }
+// 
+//         return false;
+//     }
 
-        while(true){
-            $c = $s[0];
-            $s = substr($s, 1);
-            $i = $this->match($c, $s);
-            if($i === false){
-                return false;
-            }
-
-            $s = substr($s, 0, $i) . substr($s, $i + 1);
-            if(empty($s)){
-                break;
-            }
-        }
-
-        return true;
-    }
-
-    function match($c, $s)
+    function isValid($s)
     {
-        $map = [
-            '(' => ')',
-            '{' => '}',
-            '[' => ']'
-        ];
+        $stack = [];
 
-        if($s[0] === $map[$c]) {
-            return 0;
-        }
-
-        $offset = 0;
+        $map = ['(' => ')', '{' => '}', '[' => ']'];
         for($i = 0, $len = strlen($s); $i < $len; $i++) {
-            if(in_array($s[$i], ['(', '{', '['], true)) {
-                $offset += 2;
+            if($map[$s[$i]]) {
+                $stack[] = $map[$s[$i]];
+                continue;
             }
+            
+            // if(in_array($s[$i], $map, true)) {
+                if($stack === []) {
+                    return false;
+                }
 
-            if($s[$i] === $map[$c] && $offset === $i) {
-                return $offset;
-            }
+                $last = array_pop($stack);
+                if($s[$i] !== $last) {
+                    return false;
+                }
+            // }
         }
 
-        return false;
+        return $stack === [];
     }
 }
 
